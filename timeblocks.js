e32b70b53ee4e470077391decf7fd512b7d7e713
Austin Rohn <Austinrohn19@gmail.com>
2021-06-28 00:51:56 -0400

    update3

--- timeblocks.js
+++ timeblocks.js
@@ -4,4 +4,15 @@ function createTimeblock(time, text, index) {
         <div class="row time-block">
             <div class="col-1 hour">${time.display}</div>
         </div>
-        ');
+    ');
+
+        const timeblockInput = $(`
+        <textarea name="time-block" class="col-9 description">${text}</textarea>
+    `);
+
+    const currentHour = moment().hour();
+         if (time.value < currentHour) timeblockInput.addClass("past");
+  
+        else if (time.value === currentHour) timeblockInput.addClass("present");
+    
+        else timeblockInput.addClass("future");
